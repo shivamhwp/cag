a790f34b3fa0f3b990244bb80a08fe469f00c0bd
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2025-10-26 15:30:04 +0530

    ooohhhh animations

--- src/components/ui/TopNav.tsx
+++ src/components/ui/TopNav.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from './ModeToggle'
 
 export function TopNav() {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-background">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-transparent">
       <div className="flex items-center justify-between px-6 py-4">
           <div></div>
             <ModeToggle />
